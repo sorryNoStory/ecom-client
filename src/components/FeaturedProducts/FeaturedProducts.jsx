b3b390b95a2b11508aa0c81c0d74fcb2936a364a
sorryNoStory <Medusa@192.168.1.5>
2023-03-29 14:37:59 +0800

    Initial commit

---
+++ src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Card from "../Card/Card";
+import "./FeaturedProducts.scss";
+import useFetch from "../../hooks/useFetch";
+
+const FeaturedProducts = ({ type }) => {
+  const { data, loading, error } = useFetch(
+    `/products?populate=*&[filters][type][$eq]=${type}`
+  );
+
+  return (
+    <div className="featuredProducts">
+      <div className="top">
+        <h1>{type} products</h1>
+        <p>
+        Experience the best of our online store with our Featured Products. Discover unique and trending items handpicked by our team of experts. Elevate your shopping experience today!</p>
+      </div>
+      <div className="bottom">
+        {error
+          ? "Something went wrong!"
+          : loading
+          ? "loading"
+          : data?.map((item) => <Card item={item} key={item.id} />)}
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedProducts;
